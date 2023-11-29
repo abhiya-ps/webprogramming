a9a00c26673cd5e8d92ee6393bd9ea6693867f16
abhiya-ps <147086684+abhiya-ps@users.noreply.github.com>
2023-11-29 11:13:27 +0530

    Add files via upload

---
+++ elctric.php
@@ -0,0 +1,48 @@
+<html>
+    <body>
+        <h1>Electricity Bill </h1>
+        <form method="post"action="#">
+            Consumer ID:<input type="number"name="id"><br>
+            Name:<input type="text"name="name"><br>
+            Previous Reading:<input type="number"name="prev"><br>
+            Present Reading:<input type="number"name="pres"><br><br><input type="submit" name="sub">
+        </form>
+         <?php
+         if(isset($_POST["sub"]))
+         {
+            $id=$_POST['id'];
+            $name=$_POST['name'];
+            $prev=$_POST['prev'];
+            $pres=$_POST['pres'];
+            $units=$pres-$prev;
+            echo"--------------------------------------------------------------------";
+            echo"<h3>Kerala State Electricfity Board(KSEB)</h3>";
+            echo"<br><br>";
+            echo"Consumer ID:".$id."<br>";
+            echo "Consumer name:".$name."<br>";
+            echo"Units Consumed:".$units."<br>";
+            if($units<=100)
+            {
+                $amt=$units*3;
+            }
+            else if($units>100 && $units<=200)
+            {
+    
+                 $amt=$units*4;
+            }
+            else if($units>200 && $units<=300)
+            {
+                $amt=$units*5;
+            }
+            else
+            {
+                $amt=$units*5;
+            }
+            echo"<h4>Total:Rs.".($amt)."</h4><br>";
+            echo"-----------------------------------------------------------------------";
+        }
+        ?>
+        </body>
+        </html>  
+
+        
